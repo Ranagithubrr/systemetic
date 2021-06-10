ef021a99cce96466f756b7bbde8e1421c7ec3876
Masud Rana <mr6430119@gmail.com>
2021-06-10 09:48:44 +0600

    Add files via upload

---
+++ main.js
@@ -0,0 +1,8 @@
+$(document).ready(function () {
+    
+          $('#main-menu').meanmenu({
+              meanMenuContainer: '#menu-sec' ,
+              meanScreenWidth: 992,
+          });
+
+        });
